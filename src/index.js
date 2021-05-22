b2e0444a5e7ab796d4207d7a39b74d1a07fef36c
pmqueiroz <pedroqueiroz593@gmail.com>
2021-05-22 15:48:08 -0300

    pimba

--- src/index.js
+++ src/index.js
@@ -3,32 +3,40 @@ const input = document.getElementsByTagName('bc-require')[0].getElementsByTagNam
 const messageContainers = document.getElementsByClassName('chat-line__bubble')
 const avatarContainers = document.getElementsByClassName('chat-line__avatar')
 
-Object.entries(avatarContainers).map((avatarContainerArr) => {
-   const avatarContainer = avatarContainerArr[1].getElementsByTagName('img')[0]
-   avatarContainer.src = "https://i.pinimg.com/originals/51/f6/fb/51f6fb256629fc755b8870c801092942.png"
-   avatarContainer.srcset = "https://i.pinimg.com/originals/51/f6/fb/51f6fb256629fc755b8870c801092942.png"
-})
+function sanitizeElements(input) {
+   function sanitize() {
+      let indexStart = input.indexOf('<')
+      let indexEnd = input.indexOf('>')
+      const arr = Array.from(input)
+      arr.splice(indexStart, (indexEnd - indexStart) + 1)
+      input = arr.join('')
+   }
+
+   while (input.indexOf('<') >= 0) {
+      sanitize()
+   }
+
+   return input.trim()
+}
 
 function createMessage(previousMessageValue, messageValue) {
-   const message = document.createElement('blockquote')
+   const message = document.createElement('div')
    const br = document.createElement('br')
-   const previousMessage = document.createTextNode(previousMessageValue)
+   const previousMessage = document.createTextNode(sanitizeElements(previousMessageValue).replace('&nbsp;', ''))
    const messageText = document.createTextNode(`>>> ${messageValue}`)
 
    message.appendChild(previousMessage)
    message.appendChild(br)
-   message.appendChild(br)
    message.appendChild(messageText)
 
+   input.innerHTML = ''
    input.appendChild(message)
-
-   input.dispatchEvent(new KeyboardEvent('keydown',{'key':'b'}))
 }
 
 Object.entries(messageContainers).map((messageContainerArr) => {
    const messageContainer = messageContainerArr[1]
    
-   messageContainer.getElementsByClassName('chat-line__author')[0].innerHTML = "Fulano"
+   const author = messageContainer.getElementsByClassName('chat-line__author')[0].innerHTML
 
    const messageBody = messageContainer.getElementsByClassName('chat-line__body')[0].innerHTML
    const metaContainer = messageContainer.getElementsByClassName('chat-line__meta')[0]
@@ -40,7 +48,8 @@ Object.entries(messageContainers).map((messageContainerArr) => {
    metaContainer.appendChild(button)
 
    button.addEventListener("click", function() {
-      createMessage(messageBody, '')
+      const currentMessage = sanitizeElements(String(document.getElementsByTagName('bc-require')[0].getElementsByTagName('trix-editor')[0].innerHTML))
+      createMessage(`${author}: ${messageBody}`, currentMessage)
    })
 })
 
